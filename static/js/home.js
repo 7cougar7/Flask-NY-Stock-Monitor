4d0310b4335c0716f68f3b06c462a15f29c56f5d
Tilo Garcia <tilogarcia1@gmail.com>
2018-02-16 04:25:48 +0000

    Added Top Bar Logo and Put simple api data into Side Bar

--- static/js/home.js
+++ static/js/home.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
+      $(document).ready(function() {
 
-    $("button").click(function(){
-        $("#div1").load("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=KO&outputsize=compact&apikey=XB3FP5FNA3EUUPGH", function(data){
-                var my_json=data;
-                alert(my_json);
-            });
-    });
-});
+
+                $("#sideMarket1").load("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=KO&outputsize=compact&apikey=XB3FP5FNA3EUUPGH", function(json, status) {
+                    //alert(status)
+                    var data = json;
+                    var data1 = JSON.parse(data);
+                    $("#sideMarket1").replaceWith("Coca-Cola's Opening Price for 2/15/18: $"+(data1["Time Series (Daily)"]["2018-02-15"]["1. open"]));
+
+                    });
+                });
