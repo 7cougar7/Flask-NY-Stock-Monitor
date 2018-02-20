b7b65e13a7f07b6029ef31eece49154364ff96a3
Tilo Garcia <tilogarcia1@gmail.com>
2018-02-20 23:18:54 +0000

    Added Current price change and fixed occasional JSON Index Bug

--- static/js/home.js
+++ static/js/home.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
     }
 }
 
-    var date =  d.getFullYear() + '-' + ((d.getMonth()+1)<10 ? '0' : '') + (d.getMonth()+1) + '-' + (d.getDate()<10 ? '0' : '') + (d.getDate()-is_weekend(date1));
-    alert('weekend: '+is_weekend(date1));
+   // var date =  d.getFullYear() + '-' + ((d.getMonth()+1)<10 ? '0' : '') + (d.getMonth()+1) + '-' + (d.getDate()<10 ? '0' : '') + (d.getDate()-is_weekend(date1));
+   // alert('weekend: '+is_weekend(date1));
 
 var dateTime;
 var data;
@@ -27,28 +27,31 @@ var data3;
 var openingPrice=0;
 var currentPrice=0;
 
-                $("#sideMarket2").load("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=KO&interval=1min&apikey=XB3FP5FNA3EUUPGH",function(json){
 
-                 data2 = json;
 
+                $("#sideMarket1 a").load("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=KO&outputsize=compact&apikey=XB3FP5FNA3EUUPGH", function(json, status) {
+                   // alert(status)
+                    data = json;
+                    data1 = JSON.parse(data);
+                     date=(data1["Meta Data"]["3. Last Refreshed"]);
+                    openingPrice = parseFloat(data1["Time Series (Daily)"][date]["1. open"]);
+                    $("#sideMarket1 a").replaceWith("Coca-Cola's Opening Price for "+date+ ": $"+openingPrice);
+                    //alert("1");
+                   // $("#sideMarket3 a").replaceWith(parseFloat(currentPrice)-parseFloat(openingPrice));
+                    });
+
+                $("#sideMarket2 a").load("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=KO&interval=1min&apikey=XB3FP5FNA3EUUPGH",function(json1){
+                 data2 = json1;
+                //alert(status1)
                     data3 = JSON.parse(data2);
                     //alert("2");
                     dateTime=(data3["Meta Data"]["3. Last Refreshed"]);
-                    alert("dateTime: "+dateTime);
+                    //alert("dateTime: "+dateTime);
                     currentPrice = parseFloat(data3["Time Series (1min)"][dateTime]["1. open"]);
-                    $("#sideMarket2").replaceWith("Coca-Cola's Opening Price for "+date+ ": $"+currentPrice);
+                    $("#sideMarket2 a").replaceWith("Coca-Cola's Current Price for "+dateTime+ ": $"+currentPrice);
+                    $("#sideMarket3 a").replaceWith("Change in Price from opening: $"+(parseFloat(currentPrice)-parseFloat(openingPrice)));
 
                 });
-
-                $("#sideMarket1").load("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=KO&outputsize=compact&apikey=XB3FP5FNA3EUUPGH", function(json) {
-                    //alert(status)
-                    data = json;
-                    data1 = JSON.parse(data);
-                    openingPrice = parseFloat(data1["Time Series (Daily)"][date]["1. open"]);
-                    $("#sideMarket1").replaceWith("Coca-Cola's Opening Price for "+date+ ": $"+openingPrice);
-                    alert("1");
-                    });
-
 
 
 //parseFloat(data1["Time Series (Daily)"][date]["1. open"])-
