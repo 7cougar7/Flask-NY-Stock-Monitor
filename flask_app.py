a8e52dc57f440433845fd6d7b0c7100b580f39ab
Tilo Garcia <tilogarcia1@gmail.com>
2018-04-12 21:43:01 +0000

    Implementing MySQL Database

--- flask_app.py
+++ flask_app.py
@@ -2,28 +2,26 @@ from flask import render_template
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="7cougar7",
-    password="TiloSQLDB",
-    hostname="7cougar7.mysql.pythonanywhere-services.com",
-    databasename="7cougar7$symbols",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+#SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+#    username="7cougar7",
+#    password="TiloSQLDB",
+#    hostname="7cougar7.mysql.pythonanywhere-services.com",
+#    databasename="7cougar7$symbols",
+#)
+#app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+#app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#db = SQLAlchemy(app)
 
-class Symbol(db.Model):
+#class Symbol(db.Model):
 
-    __tablename__ = "symbols"
+ #   __tablename__ = "symbols"
 
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-
-symbols= []
+  #  id = db.Column(db.Integer, primary_key=True)
+  #  content = db.Column(db.String(4096))
 
 @app.route('/', methods=["GET"])
 def index():
